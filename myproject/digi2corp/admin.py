a7146f9e437d00a0a9c75f760c1dc72b7993bc1e
Amjadali168 <amjadali1682002@gmail.com>
2024-05-10 12:12:13 +0530

    upload cv

--- myproject/digi2corp/admin.py
+++ myproject/digi2corp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Contact , Profile, Upload_resume
+from .models import Contact , Profile, Upload_cv
 
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(Profile)
-admin.site.register(Upload_resume)
+# admin.site.register(Upload_resume)
+admin.site.register(Upload_cv)
